a766238896a4df8acde2354b1a447672b40bb3ee
mukeshxtech <mukeshkmr.mukku@gmail.com>
2024-11-28 13:39:32 +0530

    bill modification

--- rollercoaster_ticketing.py
+++ rollercoaster_ticketing.py
@@ -1,38 +1,24 @@
 print("Welcome to rollercoaster")
 height = int(input("What is your height in cm? "))
-child_ticket = 20
-youth_ticket = 30
-adult_ticket = 40
+bill = 0
 
 if height >= 120:
     print("You can ride")
     age = int(input("What is your age? "))
     if age <= 12:
-        print(f"Child tickets are ₹{child_ticket}")
-        child_photo = input("Do you want photos? yes or no?")
-        if child_photo=='yes':
-            print(f"Your total bill is ₹{child_ticket + 10}")
-        elif child_photo=='no':
-            print(f"Your total bill is ₹{child_ticket}")
-        else:
-            print("yes or no?")
+        print(f"Child tickets are ₹20")
+        bill = 20
     elif 12 < age <=18:
-        print(f"Youth tickets are ₹{youth_ticket}")
-        youth_photo = input("Do you want photos? yes or no?")
-        if youth_photo == 'yes':
-            print(f"Your total bill is ₹{youth_ticket + 10}")
-        elif youth_photo == 'no':
-            print(f"Your total bill is ₹{youth_ticket}")
-        else:
-            print("yes or no?")
+        print(f"Youth tickets are ₹30")
+        bill = 30
     else:
-        print(f"Adult tickets are ₹{adult_ticket}")
-        adult_photo = input("Do you want photos? yes or no?")
-        if adult_photo == 'yes':
-            print(f"Your total bill is ₹{adult_ticket + 10}")
-        elif adult_photo == 'no':
-            print(f"Your total bill is ₹{adult_ticket}")
-        else:
-            print("yes or no?")
+        print(f"Adult tickets are ₹40")
+        bill = 40
+
+    wants_photo = input("Do you want to have a photo take? type y for yes and n for no ")
+    if wants_photo == 'y':
+        # Add ₹10 to their bill
+        bill += 10
+    print(f"Your finale bill is {bill}")
 else:
-    print("Sorry you have to grow taller before you can ride. ")
+    print("Sorry you have to grow taller before you can ride.")
